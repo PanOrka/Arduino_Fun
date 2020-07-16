25f4d4bf147efb142a690744548bd076a61c99a6
Eryk <37209877+PanOrka@users.noreply.github.com>
2020-07-16 13:04:18 +0200

    + binary_clock_with_8bit_register

---
+++ binary_clock_with_8bit_register/binary_clock_with_8bit_register.ino
@@ -0,0 +1,49 @@
+const short SER_pin = 2;
+const short SRCLK_pin = 4;
+const short RCLK_pin = 3;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(SER_pin, OUTPUT);
+  pinMode(RCLK_pin, OUTPUT);
+  pinMode(SRCLK_pin, OUTPUT);
+}
+
+const short led_arr[] = {1, 2, 4};
+
+void write_one() {
+  digitalWrite(SER_pin, HIGH);
+  digitalWrite(SRCLK_pin, LOW);
+  digitalWrite(SRCLK_pin, HIGH);
+}
+
+void write_zero() {
+  digitalWrite(SER_pin, LOW);
+  digitalWrite(SRCLK_pin, LOW);
+  digitalWrite(SRCLK_pin, HIGH);
+}
+
+void load_register() {
+  digitalWrite(RCLK_pin, LOW);
+  digitalWrite(RCLK_pin, HIGH);
+}
+
+void put_num(short num) {
+  for (int i=0; i<3; i++) {
+    if ((num & led_arr[i]) == led_arr[i]) {
+      write_one();
+    } else {
+      write_zero();
+    }
+  }
+  load_register();
+}
+
+unsigned short time;
+
+void loop() {
+  put_num(time);
+  delay(1000);
+  ++time;
+}
